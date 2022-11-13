a75685fc3bf9ab2c4f3978da49f888b1515ad46b
Petefolio <petelucey11@gmail.com>
2022-11-13 05:28:24 +0000

    Added on-screen instructions

--- CA1.cpp
+++ CA1.cpp
@@ -5,6 +5,14 @@ struct Player { //struct for player
     Color color;
 };
 
+struct Bullets{ //struct for bullets. Credit to https://github.com/naoisecollins/scarfyJump/blob/master/scarfyJump.cpp.
+	Rectangle rect;
+	Vector2 speed; 
+	Color color;
+	bool active;
+};
+//End of credit.
+
 int main() {
 const int windowWidth{500};
 const int windowHeight{500};
@@ -12,7 +20,6 @@ int player_posX = 250;
 int player_posY = 250;
 int life_counter = 3;
 int num_bullets{50};
-
 InitWindow(800, 500, "Game");
 SetTargetFPS(60);
 
@@ -24,6 +31,18 @@ player.rect.x = windowWidth/2;
 player.rect.y = windowHeight-player.rect.height;
 player.speed.x = 10;
 player.speed.y = 0;
+
+Bullets bullet[num_bullets];
+for (int i = 0; i<num_bullets; i++){
+bullet[i].rect.height= 10;
+bullet[i].rect.width = 10;
+bullet[i].color = BLACK; //changed the color of the bullets from green to black 
+bullet[i].rect.x = (windowWidth/2)+(player.rect.width/2)-(bullet[i].rect.width/2);
+bullet[i].rect.y = (windowHeight-player.rect.height)-(bullet[i].rect.height);
+bullet[i].speed.x = 50;
+bullet[i].speed.y = -10;
+bullet[i].active = false;}
+int shootRate = 0;
 
 Music music = LoadMusicStream("resources/bg_music.wav"); 
 Sound sound = LoadSound("resources/pistol.wav");
@@ -54,7 +73,7 @@ Rectangle door = {GetScreenWidth()/2.0f, GetScreenHeight()- 500, 60, 10 };
 bool collision{}; //collision bool
 bool have_key{}; //bool for having key
 bool game_end{false}; //bool to end game
-bool game_begin{false};
+
 
 while(!WindowShouldClose()){
 
@@ -86,6 +105,45 @@ if(CheckCollisionRecs(player.rect,pit)){
 if(CheckCollisionRecs(player.rect, door) && have_key){
     game_end = true; //end the game if the player passes through the door carrying the
 }
+
+for (int i = 0; i < num_bullets; i++) //Shoot code. Credit to https://github.com/naoisecollins/scarfyJump/blob/master/scarfyJump.cpp.
+    {
+        if (bullet[i].active)
+            DrawRectangleRec(bullet[i].rect, bullet[i].color);
+    }
+
+    if (IsKeyDown(KEY_SPACE))
+    {
+        shootRate += 5;
+        for (int i = 0; i <num_bullets; i++)
+        {
+            if (!bullet[i].active && shootRate % 40 == 0)
+            {
+				PlaySound(sound);
+                bullet[i].rect.x = player.rect.x;
+                bullet[i].rect.y = player.rect.y + player.rect.height / 4;
+                bullet[i].active = true;
+				break;
+            }
+        }
+    }
+
+
+    for (int i = 0; i < num_bullets; i++)
+    {
+        if (bullet[i].active)
+        {
+           
+            bullet[i].rect.x += bullet[i].speed.y; //Change made: added speed to the X position of the bullet instead of the Y.
+
+            if (bullet[i].rect.y <= 0) 
+            {
+                bullet[i].active = false;
+                shootRate = 0;
+            }
+        }
+    }
+//End of credit.
 
 if (((boxA.x + boxA.width) >= GetScreenWidth()) || (boxA.x <= 0)) boxASpeedX *= -1;
 //Enemy movement. Credit to: https://www.raylib.com/examples.html "collision area". End of credit.
@@ -126,10 +184,12 @@ DrawRectangleRec(boxD, RED);
 DrawRectangleRec(boxE, RED);
 DrawRectangleRec(pit, PURPLE);
 DrawRectangleRec(door, GREEN);
+DrawText("Collect the yellow key and", 20, 80, 20, BLACK);
+DrawText("escape through the green door!", 20, 100, 20, BLACK);
 }
 
 if (!have_key) {
-DrawRectangleRec(key, YELLOW); //have the key disappear if the player picks it up, and hve it be drawn if they have not.
+DrawRectangleRec(key, YELLOW); //have the key disappear if the player picks it up, and be drawn if they have not.
 }
 
 EndDrawing();
