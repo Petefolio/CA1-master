68dfb34dd4d0ecd3b11de33905c4ea2f380225bf
Petefolio <petelucey11@gmail.com>
2022-11-13 06:04:22 +0000

    added bg music, sfx, deleted unneeded resources

--- CA1.cpp
+++ CA1.cpp
@@ -10,9 +10,8 @@ const int windowWidth{500};
 const int windowHeight{500};
 int player_posX = 250;
 int player_posY = 250;
-int life_counter = 3;
-int num_bullets{50};
 InitWindow(800, 500, "Game");
+InitAudioDevice();
 SetTargetFPS(60);
 
 Player player;
@@ -25,23 +24,23 @@ player.speed.x = 10;
 player.speed.y = 0;
 
 Music music = LoadMusicStream("resources/bg_music.wav"); 
-Sound sound = LoadSound("resources/pistol.wav");
+Sound sound = LoadSound("resources/collect.wav");
 PlayMusicStream(music);
 
-Rectangle boxA = { 10, GetScreenHeight()/2.0f - 50, 200, 100 }; //rectangle enemy placeholders
-int boxASpeedX = 4; //Credit to: https://www.raylib.com/examples.html "collision area"
+Rectangle enemy = { 10, GetScreenHeight()/2.0f - 50, 200, 100 }; 
+int enemySpeedX = 4; //Credit to: https://www.raylib.com/examples.html "collision area"
 //End of credit
 
-Rectangle boxB = { 100, GetScreenHeight()/4.0f - 100, 50, 50 };
+Rectangle obstacleA = { 100, GetScreenHeight()/4.0f - 100, 50, 50 };
 int boxBSpeedY = 4;
 
-Rectangle boxC = { 650, GetScreenHeight()/4.0f - 100, 50, 50 };
+Rectangle obstacleB = { 650, GetScreenHeight()/4.0f - 100, 50, 50 };
 int boxCSpeedY = 4;
 
-Rectangle boxD = { 600, GetScreenHeight()/1.1f - 100, 50, 50 };
+Rectangle obstacleC = { 600, GetScreenHeight()/1.1f - 100, 50, 50 };
 int boxDSpeedY = 4;
 
-Rectangle boxE = { 150, GetScreenHeight()/1.1f - 100, 50, 50 };
+Rectangle obstacleD = { 150, GetScreenHeight()/1.1f - 100, 50, 50 };
 int boxESpeedY = 4;
 
 Rectangle pit = { 360, GetScreenHeight()/2.5f - 100, 80, 80 };
@@ -57,25 +56,28 @@ bool game_end{false}; //bool to end game
 
 while(!WindowShouldClose()){
 
-if(CheckCollisionRecs(player.rect,boxA)){ //collision detection for enemies and obstacles
+UpdateMusicStream(music); 
+
+if(CheckCollisionRecs(player.rect,enemy)){ //collision detection for enemies and obstacles
 		collision = true;
 	}
-if(CheckCollisionRecs(player.rect,boxB)){
+if(CheckCollisionRecs(player.rect,obstacleA)){
 		collision = true;
 	}
-if(CheckCollisionRecs(player.rect,boxC)){
+if(CheckCollisionRecs(player.rect,obstacleB)){
 		collision = true;
 	}
-if(CheckCollisionRecs(player.rect,boxD)){
+if(CheckCollisionRecs(player.rect,obstacleC)){
 		collision = true;
 	}
-if(CheckCollisionRecs(player.rect,boxE)){
+if(CheckCollisionRecs(player.rect,obstacleD)){
 		collision = true;
 	}
 if(CheckCollisionRecs(player.rect,pit)){
 		collision = true;
 	}
 if(CheckCollisionRecs(player.rect,key)){ //check to see if the player has picked up the key
+        PlaySound(sound);
         have_key = true;
 }
 
@@ -86,7 +88,7 @@ if(CheckCollisionRecs(player.rect, door) && have_key){
     game_end = true; //end the game if the player passes through the door carrying the
 }
 
-if (((boxA.x + boxA.width) >= GetScreenWidth()) || (boxA.x <= 0)) boxASpeedX *= -1;
+if (((enemy.x + enemy.width) >= GetScreenWidth()) || (enemy.x <= 0)) enemySpeedX *= -1;
 //Enemy movement. Credit to: https://www.raylib.com/examples.html "collision area". End of credit.
 
 if (IsKeyDown(KEY_D))
@@ -107,7 +109,7 @@ if (IsKeyDown(KEY_W))
 }
 BeginDrawing();
 
-boxA.x += boxASpeedX; //Enemy movement. Credit to: https://www.raylib.com/examples.html "collision area". End of credit
+enemy.x += enemySpeedX; //Enemy movement. Credit to: https://www.raylib.com/examples.html "collision area". End of credit
 ClearBackground(WHITE);
 if (collision) {
  DrawText("Game Over!",GetScreenHeight()/2.0f, GetScreenHeight()/2.0f, 50, PURPLE); //game over screen
@@ -118,11 +120,11 @@ else if (game_end){ //else if statement to allow for both the game over and game
 else{
 
 DrawRectangleRec(player.rect,player.color); //draw the sprites if the game is not over
-DrawRectangleRec(boxA, GOLD);
-DrawRectangleRec(boxB, RED);
-DrawRectangleRec(boxC, RED);
-DrawRectangleRec(boxD, RED);
-DrawRectangleRec(boxE, RED);
+DrawRectangleRec(enemy, GOLD);
+DrawRectangleRec(obstacleA, RED);
+DrawRectangleRec(obstacleB, RED);
+DrawRectangleRec(obstacleC, RED);
+DrawRectangleRec(obstacleD, RED);
 DrawRectangleRec(pit, PURPLE);
 DrawRectangleRec(door, GREEN);
 DrawText("Collect the yellow key and", 20, 80, 20, BLACK);
